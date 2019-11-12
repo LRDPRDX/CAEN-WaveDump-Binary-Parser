c8d23a372d68dabfe87046349405646373962b0a
lrdprdx <paradox1859@gmail.com>
2019-11-12 13:23:26 +0700

    NaI(Tl) data added

--- example/ROOT/create_tree/CreateTree.C
+++ example/ROOT/create_tree/CreateTree.C
@@ -4,12 +4,13 @@
 
 void CreateTree()
 {
-    CaenTreeCreator tc( caen::Board::N6720,
-                        "../../../data/",
-                        "./",
-                        "myFirstTree" );
+    CaenTreeCreator tc( caen::Board::N6720, //N6720 
+                        "../../../data/",   //data dir
+                        "./",               //place the TTree in the current dir
+                        "myFirstTree" );    //name the TTree as "myFirstTree"
 
-    tc.SetIntervals( 0.03, 0.03, 1 );
+    tc.SetIntervals( 0.1, 0.1, 2 );//baseline - from 0 to 50 ns; integral - from 100 to 2000 ns
 
-    tc.CreateTree();
+    //create a TTree only from the one directory "mult" in the data directory
+    tc.CreateTree( CaenTreeCreator::INCLUDE, "mult" );
 }
